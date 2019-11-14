c47716231888b8206069dbb314f1418f82d9d10a
Carlos Figueiras Valverde <cfigueirasvalverde@danielcastelao.org>
2019-11-14 13:42:36 +0100

    boletin10_1

---
+++ Boletin10_1/src/boletin10_1/Boletin10_1.java
@@ -0,0 +1,14 @@
+package boletin10_1;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin10_1 {
+
+    
+    public static void main(String[] args) {
+        Juego tomate = new Juego();
+        tomate.iniciarJuego();
+    }
+}
