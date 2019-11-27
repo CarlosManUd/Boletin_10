06a9c5004e442337dd0c851772639b57d6feb2f3
Carlos Figueiras Valverde <cfigueirasvalverde@danielcastelao.org>
2019-11-27 13:41:16 +0100

    boletin10_2

---
+++ Boletin10_2/src/boletin10_2/Boletin10_2.java
@@ -0,0 +1,18 @@
+ 
+package boletin10_2;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin10_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Juego lol = new Juego();
+       lol.iniciarJuego();
+    }
+    
+}
